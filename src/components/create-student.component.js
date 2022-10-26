281db2143cad4c731523a16f5af321131c9bde72
Bryan Quintero <bryanquinterog@gmail.com>
2022-10-26 18:53:09 -0500

    primeros componentes

---
+++ src/components/create-student.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react' //obligatoria al crear componentes
+
+export default class CreateStudent extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
---
+++ tutorial.md
@@ -0,0 +1,4 @@
+# components
+partes de la pagina se hace con .componente.js
+snipper rcc = react class student
+snipper se encarga de reducir codigo hacerlo mas rapido
