e0c17ddfddd751feca697bcb54dda04e941df5ff
Bryan Quintero <bryanquinterog@gmail.com>
2022-11-08 18:12:05 -0500

    base de datos

---
+++ backend/database/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:
+        "mongodb+srv://bryanquintero:crudstackmern@clustermernstack.nvrixuo.mongodb.net/reactdb?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+};
